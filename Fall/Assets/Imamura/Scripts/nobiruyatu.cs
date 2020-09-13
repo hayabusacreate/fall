7d3151c2d529d2c3fb6daf5174d55c9ab0bccec5
Imamura728 <image50804650@gmail.com>
2020-09-13 18:14:04 +0900

    タイトルシーンの演出を追加、アニメーションの作製、のびるやつ

---
+++ Fall/Assets/Imamura/Scripts/nobiruyatu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nobiruyatu : MonoBehaviour
+{
+    Material nobi;
+    float scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nobi = transform.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scale = transform.localScale.x;
+        nobi.mainTextureScale = new Vector2(scale,1);
+    }
+}
