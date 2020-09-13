f28257767565b833b3bde0014a154353337c584f
Imamura728 <image50804650@gmail.com>
2020-09-13 15:06:38 +0900

    モデルとかの調整

---
+++ Fall/Assets/Imamura/Scripts/ObjCont.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjCont : MonoBehaviour
+{
+    public GameObject obj;
+    float scale,rot;
+    float time, tortalTime;
+    public GameObject Camera,camP1, camP2,titleLogo,blocks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tortalTime = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.J))
+        {
+            time += Time.deltaTime;
+            if(time <= 1)
+            {
+                scale = easing.BackOut(time, tortalTime, 0, 1, 1.5f);
+                obj.transform.localScale = new Vector3(scale, scale, scale);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            time += Time.deltaTime;
+            if (time <= 2)
+            {
+                rot = easing.BackOut(time, 2, -90, -80,1.5f);
+                obj.transform.rotation = Quaternion.Euler(new Vector3(rot, obj.transform.rotation.y, obj.transform.rotation.z));
+            }
+            else if (time <= 6 && time > 2)
+            {
+                rot = easing.BackOut(time-2, 4, -80,30, 1.2f);
+                obj.transform.rotation = Quaternion.Euler(new Vector3(rot, obj.transform.rotation.y, obj.transform.rotation.z));
+                Camera.transform.position = easing3D.SineOut(time - 2, 4, camP1.transform.position, camP2.transform.position);
+            }
+            else if (time <= 8 && time > 6)
+            {
+                rot = easing.BackOut(time - 6, 2, -80, 30, 1.2f);
+                blocks.transform.position = easing3D.SineOut(time - 6, 2, new Vector3(0,0,0), new Vector3(0, 7.5f, 0));
+                titleLogo.transform.position = easing3D.SineOut(time - 6, 2, new Vector3(0, 0, 0), new Vector3(0, 13.5f, 0));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            time = 0;
+            
+        }
+    }
+}
---
+++ Fall/Assets/Imamura/Scripts/walk.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walk : MonoBehaviour
+{
+    public GameObject L;
+    public GameObject R;
+    bool Lup, Rup,st;
+    float upTime,value;
+    Vector3 Lpos, Rpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        value = 0.3f;
+        Lpos = L.transform.position;
+        Rpos = R.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        upTime += Time.deltaTime;
+        //if(upTime <= 0.5f && st == true)
+        //{
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y + value, L.transform.position.z);
+        //    R.transform.position = new Vector3(R.transform.position.x, R.transform.position.y - value, R.transform.position.z);
+        //}
+        //if (upTime <= 0.5f && st == false)
+        //{
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y + value, L.transform.position.z);
+        //}
+        //if (upTime <= 1&& upTime > 0.5f)
+        //{
+        //    R.transform.position = new Vector3(R.transform.position.x, R.transform.position.y + value, R.transform.position.z);
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y - value, L.transform.position.z);
+        //}
+        //if (upTime <= 1.5f && upTime > 1)
+        //{
+        //    R.transform.position = new Vector3(R.transform.position.x, R.transform.position.y - value, R.transform.position.z);
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y + value, L.transform.position.z);
+
+        //}
+        //if (upTime <= 2 && upTime > 1.5f)
+        //{
+        //    R.transform.position = new Vector3(R.transform.position.x, R.transform.position.y + value, R.transform.position.z);
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y - value, L.transform.position.z);
+        //}
+        //if (upTime <= 2.5f && upTime > 2)
+        //{
+        //    R.transform.position = new Vector3(R.transform.position.x, R.transform.position.y - value, R.transform.position.z);
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y + value, L.transform.position.z);
+        //}
+        //if (upTime <= 3 && upTime > 2.5f)
+        //{
+        //    R.transform.position = new Vector3(R.transform.position.x, R.transform.position.y + value, R.transform.position.z);
+        //    L.transform.position = new Vector3(L.transform.position.x, L.transform.position.y - value, L.transform.position.z);
+        //}
+        //if ( upTime > 3)
+        //{
+        //    st = true;
+        //    upTime = 0;
+        //}
+        if (upTime <= 0.5f && st == true)
+        {
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y+ value, L.transform.position.z);
+            R.transform.position = new Vector3(R.transform.position.x, Rpos.y, R.transform.position.z);
+        }
+        if (upTime <= 0.5f && st == false)
+        {
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y + value, L.transform.position.z);
+        }
+        if (upTime <= 1 && upTime > 0.5f)
+        {
+            R.transform.position = new Vector3(R.transform.position.x, Rpos.y + value, R.transform.position.z);
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y, L.transform.position.z);
+        }
+        if (upTime <= 1.5f && upTime > 1)
+        {
+            R.transform.position = new Vector3(R.transform.position.x, Rpos.y, R.transform.position.z);
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y + value, L.transform.position.z);
+
+        }
+        if (upTime <= 2 && upTime > 1.5f)
+        {
+            R.transform.position = new Vector3(R.transform.position.x, Rpos.y + value, R.transform.position.z);
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y, L.transform.position.z);
+        }
+        if (upTime <= 2.5f && upTime > 2)
+        {
+            R.transform.position = new Vector3(R.transform.position.x, Rpos.y , R.transform.position.z);
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y + value, L.transform.position.z);
+        }
+        if (upTime <= 3 && upTime > 2.5f)
+        {
+            R.transform.position = new Vector3(R.transform.position.x, Rpos.y + value, R.transform.position.z);
+            L.transform.position = new Vector3(L.transform.position.x, Lpos.y , L.transform.position.z);
+        }
+        if (upTime > 3)
+        {
+            st = true;
+            upTime = 0;
+        }
+    }
+}
