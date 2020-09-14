8be788b77328de2beb91ee7c56aca77830dcc269
Imamura728 <image50804650@gmail.com>
2020-09-14 10:58:27 +0900

    ロードシーンいじった

---
+++ Fall/Assets/Imamura/Scripts/LoadFx.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFx : MonoBehaviour
+{
+    public GameObject m;
+    float rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rot += Time.deltaTime*100;
+        m.transform.rotation = Quaternion.Euler(new Vector3(m.transform.rotation.x, m.transform.rotation.y, m.transform.rotation.z+rot));
+    }
+}
